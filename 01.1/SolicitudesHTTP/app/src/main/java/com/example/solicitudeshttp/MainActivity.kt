d98229444fae5f1f80489b56fd9f8e22e6b1839c
SergioRomero96 <sergio.nicolas777@gmail.com>
2020-11-03 23:48:04 -0300

    Solicitud HTTP con Volley

--- 01.1/SolicitudesHTTP/app/src/main/java/com/example/solicitudeshttp/MainActivity.kt
+++ 01.1/SolicitudesHTTP/app/src/main/java/com/example/solicitudeshttp/MainActivity.kt
@@ -6,8 +6,13 @@ import android.os.StrictMode
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import java.io.IOException
 import java.io.InputStream
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -18,6 +23,7 @@ class MainActivity : AppCompatActivity(), CompletadoListener {
 
         val buttonValidarRed = findViewById<Button>(R.id.btnValidarRed)
         val buttonSolicitudHTTP = findViewById<Button>(R.id.btnSolicitud)
+        val buttonVolleyHTTP = findViewById<Button>(R.id.btnSolicitudVolley)
 
         buttonValidarRed.setOnClickListener {
             //Codigo para validar red
@@ -36,11 +42,33 @@ class MainActivity : AppCompatActivity(), CompletadoListener {
                 Toast.makeText(this, "No hay conexion a internet", Toast.LENGTH_SHORT).show()
             }
         }
+
+        buttonVolleyHTTP.setOnClickListener {
+            if(Network.hayRed(this)){
+                solicitudHTTPVolley("https://www.google.com")
+            }else{
+                Toast.makeText(this, "No hay conexion a internet", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun descargaCompleta(resultado: String) {
         Log.d("descargaCompleta", resultado)
     }
 
+    //Metodo para volley
+    private fun solicitudHTTPVolley(url:String){
+        val queue = Volley.newRequestQueue(this)
+        val solicitud = StringRequest(Request.Method.GET, url, Response.Listener<String>{
+            response ->
+            try {
+                Log.d("SolicitudHTTPVolley", response)
+            }catch (e: Exception){
+
+            }
+        }, Response.ErrorListener {  })
+
+        queue.add(solicitud)
+    }
 
 }
