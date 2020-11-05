534abd4c31eaa1036b89bc784d6c29b8a66b6cac
SergioRomero96 <sergio.nicolas777@gmail.com>
2020-11-05 01:06:32 -0300

    Agregado material design a AppClima

--- 01.0/AppClima/app/src/main/java/com/example/appclima/CiudadesActivity.kt
+++ 01.0/AppClima/app/src/main/java/com/example/appclima/CiudadesActivity.kt
@@ -4,30 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
+import com.google.android.material.textfield.TextInputLayout
 
 class CiudadesActivity : AppCompatActivity() {
     val TAG = "com.example.appclima.ciudades.CIUDAD"
+    private lateinit var tvCountry: AutoCompleteTextView
+    private lateinit var tvCity:TextInputLayout
+    private lateinit var btnSearch:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
 
-        val buttonMexico = findViewById<Button>(R.id.btn_mexico)
-        val buttonBerlin = findViewById<Button>(R.id.btn_berlin)
+        tvCountry = findViewById(R.id.country)
+        tvCity = findViewById(R.id.city)
+        btnSearch = findViewById(R.id.btn_search)
 
-        buttonMexico.setOnClickListener(View.OnClickListener {
-            //Toast.makeText(this, "Ciudad de México",Toast.LENGTH_SHORT)
+        val option:Array<String> = arrayOf("Mexico", "Berlin")
+        val arrayAdapter = ArrayAdapter(this, R.layout.option_item, option)
+        // to make default value
+        //autoCompleteTextView.setText(arrayAdapter.getItem(0).toString(), false)
+
+        tvCountry.setAdapter(arrayAdapter)
+        btnSearch.setOnClickListener(View.OnClickListener {
             val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra(TAG, "3530597")
-            startActivity(intent)
+            var value = ""
+            println("ciudad: " + tvCountry.text.toString())
+            if(tvCountry.text.toString() == "Mexico") {
+                value = "3530597"
+                intent.putExtra(TAG, value)
+                startActivity(intent)
+            }else if(tvCountry.text.toString() == "Berlin") {
+                value = "2950159"
+                intent.putExtra(TAG, value)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Seleccione un pais", Toast.LENGTH_SHORT).show()
+            }
+
         })
 
-        buttonBerlin.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra(TAG, "2950159")
-            startActivity(intent)
-        })
     }
 }
